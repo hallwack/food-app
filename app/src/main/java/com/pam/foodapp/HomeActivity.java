3f753a002b3513f5cdfbd6b1fb9bd5930036cd2e
Raihan Adam <hallwack.id@gmail.com>
2021-11-05 20:51:54 +0700

    Add Logout Function

--- app/src/main/java/com/pam/foodapp/HomeActivity.java
+++ app/src/main/java/com/pam/foodapp/HomeActivity.java
@@ -1,8 +1,17 @@
 package com.pam.foodapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import java.util.ArrayList;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -11,6 +20,31 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        ArrayList<Food> foodsData = FoodData .getFoodData(getApplicationContext());
+        RecyclerView recyclerView = findViewById(R.id.list_item);
+        FoodsAdapter adapter = new FoodsAdapter(foodsData);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.app_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.button_logout:
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                this.finish();
+                break;
+            default:
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
